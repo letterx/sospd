c3b63ae63f1b2f740d04d43e35da3bd76521b846
Alex Fix <afix@cs.cornell.edu>
2013-02-25 19:39:27 -0500

    Adding implementations of various functions for SubmodularFlow as well as comments describing what everything is supposed to do

--- submodular-flow.cpp
+++ submodular-flow.cpp
@@ -1,23 +1,44 @@
 #include "submodular-flow.hpp"
 
-SubmodularFlow::SubmodularFlow() {
-    // Implement me (Alex)
-}
+SubmodularFlow::SubmodularFlow() 
+    : m_num_nodes(0),
+    m_phi_si(),
+    m_phi_it(),
+    m_labels(),
+    m_num_cliques(0),
+    m_cliques(),
+    m_neighbors()
+{ }
 
 SubmodularFlow::NodeId SubmodularFlow::AddNode(int n) {
-    // Implement me (Alex)
+    ASSERT(n >= 1);
+    NodeId first_node = m_num_nodes;
+    for (int i = 0; i < n; ++i) {
+        m_phi_si.push_back(0);
+        m_phi_it.push_back(0);
+        m_labels.push_back(-1);
+        m_neighbors.push_back(NeighborList());
+        m_num_nodes++;
+    }
+    return first_node;
 }
 
 int SubmodularFlow::GetLabel(NodeId n) const {
-    // Implement me (Alex)
+    return m_labels[n];
 }
 
 void SubmodularFlow::AddUnaryTerm(NodeId n, REAL E0, REAL E1) {
-    // Implement me (Alex)
+    m_phi_si[n] += E0;
+    m_phi_it[n] += E1;
 }
 
 void SubmodularFlow::AddClique(const CliquePtr& cp) {
-    // Implement me (Alex)
+    m_cliques.push_back(cp);
+    for (NodeId i : cp->Nodes()) {
+        ASSERT(0 <= i && i < m_num_nodes);
+        m_neighbors[i].push_back(m_num_cliques);
+    }
+    m_num_cliques++;
 }
 
 void SubmodularFlow::PushRelabel() {
--- submodular-flow.hpp
+++ submodular-flow.hpp
@@ -9,18 +9,30 @@ typedef int64_t REAL;
 class SubmodularFlow {
     public:
         typedef int NodeId;
+        typedef int CliqueId;
         struct Clique;
         typedef std::shared_ptr<Clique> CliquePtr;
         typedef std::vector<CliquePtr> CliqueVec;
 
         SubmodularFlow();
 
+        // Add n new nodes to the base set V
         NodeId AddNode(int n = 1);
+
+        // GetLabel returns 1, 0 or -1 if n is in S, not in S, or haven't 
+        // computed flow yet, respectively
         int GetLabel(NodeId n) const;
+
+        // AddUnaryTerm for node n, with cost E0 for not being in S and E1
+        // for being in S
         void AddUnaryTerm(NodeId n, REAL E0, REAL E1);
+
+        // AddClique pointed to by cp
         void AddClique(const CliquePtr& cp);
 
+        // Compute the max flow using PushRelabel
         void PushRelabel();
+        // After computing the max flow, extract the min cut
         void ComputeMinCut();
 
         /* Clique: abstract base class for user-defined clique functions
@@ -32,10 +44,15 @@ class SubmodularFlow {
          */
         class Clique {
             public:
-            Clique(const std::vector<NodeId>& nodes) : m_nodes(nodes) { }
+            Clique(const std::vector<NodeId>& nodes) 
+                : m_nodes(nodes), 
+                m_alpha_Ci(nodes.size(), 0)
+            { }
             ~Clique() = default;
 
+            // Returns the energy of the given labeling for this clique function
             virtual REAL ComputeEnergy(const std::vector<int>& labels) const = 0;
+            // Returns the exchange capacity between nodes u and v
             virtual REAL ExchangeCapacity(NodeId u, NodeId v) const = 0;
 
             const std::vector<NodeId>& Nodes() const { return m_nodes; }
@@ -43,6 +60,7 @@ class SubmodularFlow {
 
             protected:
             std::vector<NodeId> m_nodes; // The list of nodes in the clique
+            std::vector<REAL> m_alpha_Ci; // The reparameterization variables for this clique
             
             // Prohibit copying and moving clique functions, to prevent slicing
             // of derived class data
@@ -53,9 +71,16 @@ class SubmodularFlow {
         };
 
     protected:
+        typedef std::vector<CliqueId> NeighborList;
+
+        NodeId m_num_nodes;
         std::vector<REAL> m_phi_si;
         std::vector<REAL> m_phi_it;
+        std::vector<int> m_labels; 
+
+        CliqueId m_num_cliques;
         CliqueVec m_cliques;
+        std::vector<NeighborList> m_neighbors;
 };
 
 /*
