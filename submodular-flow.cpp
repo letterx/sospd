53ce7ba7ae1885fe14682f169d2aaf4c3e622096
Alex Fix <afix@cs.cornell.edu>
2013-02-25 19:10:54 -0500

    Adding stubs for implementations

---
+++ submodular-flow.cpp
@@ -0,0 +1,31 @@
+#include "submodular-flow.hpp"
+
+SubmodularFlow::SubmodularFlow() {
+    // Implement me (Alex)
+}
+
+SubmodularFlow::NodeId SubmodularFlow::AddNode(int n) {
+    // Implement me (Alex)
+}
+
+int SubmodularFlow::GetLabel(NodeId n) const {
+    // Implement me (Alex)
+}
+
+void SubmodularFlow::AddUnaryTerm(NodeId n, REAL E0, REAL E1) {
+    // Implement me (Alex)
+}
+
+void SubmodularFlow::AddClique(const CliquePtr& cp) {
+    // Implement me (Alex)
+}
+
+void SubmodularFlow::PushRelabel() {
+    // Implement me (Sam)
+}
+
+void SubmodularFlow::ComputeMinCut() { 
+    // Implement me (Sam)
+}
+
+
