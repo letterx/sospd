f20b299e17b18d1f8f8ed43207f5b42af4f47e6b
Alex Fix <afix@cs.cornell.edu>
2013-02-28 23:04:48 -0500

    Adding basic test case of flow network with 4 vars and a single clique

--- test/test-submodular-flow.cpp
+++ test/test-submodular-flow.cpp
@@ -7,7 +7,24 @@
 BOOST_AUTO_TEST_SUITE(basicSubmodularFlow)
 
 BOOST_AUTO_TEST_CASE(sanityCheck) {
-    BOOST_CHECK_EQUAL(2+2, 4);
+    typedef SubmodularFlow::NodeId NodeId;
+
+    SubmodularFlow sf;
+    sf.AddNode(4);
+
+    std::vector<NodeId> nodes = {0, 1, 2, 3};
+    const size_t numAssignments = 1 << 4;
+    std::vector<REAL> energyTable(numAssignments, 0);
+    energyTable[0xf] = -1;
+    sf.AddClique(nodes, energyTable);
+
+    sf.PushRelabel();
+    sf.ComputeMinCut();
+
+    BOOST_CHECK_EQUAL(sf.ComputeEnergy(), -1);
+    for (NodeId i = 0; i < 4; ++i) {
+        BOOST_CHECK_EQUAL(sf.GetLabel(i), 1);
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
