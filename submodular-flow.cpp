81b503890c4398e3def31fa141d71b0ffb81cc8f
Alex Fix <afix@cs.cornell.edu>
2013-02-28 21:32:53 -0500

    Fixing bugs in EnergyTableClique: needed to take alpha_Ci into account

--- submodular-flow.cpp
+++ submodular-flow.cpp
@@ -57,8 +57,8 @@ void SubmodularFlow::ComputeMinCut() {
 REAL SubmodularFlow::ComputeEnergy() const {
     REAL total = 0;
     for (NodeId i = 0; i < m_num_nodes; ++i) {
-        if (m_labels[i] == 1) total += m_c_it;
-        else total += m_c_si;
+        if (m_labels[i] == 1) total += m_c_it[i];
+        else total += m_c_si[i];
     }
     for (const CliquePtr& cp : m_cliques) {
         total += cp->ComputeEnergy(m_labels);
@@ -85,9 +85,14 @@ REAL EnergyTableClique::ExchangeCapacity(NodeId u, NodeId v) const {
     REAL min_energy = std::numeric_limits<REAL>::max();
     Assignment num_assgns = 1 << this->m_nodes.size();
     for (Assignment assgn = 0; assgn < num_assgns; ++assgn) {
+        REAL alpha_C = 0;
+        for (size_t i = 0; i < this->m_alpha_Ci.size(); ++i) {
+            if (assgn & (1 << i)) alpha_C += this->m_alpha_Ci[i];
+        }
         if (assgn & (1 << u_idx) && !(assgn & (1 << v_idx))) {
             // then assgn is a set separating u from v
-            if (m_energy[assgn] < min_energy) min_energy = m_energy[assgn];
+            REAL energy = m_energy[assgn] - alpha_C;
+            if (energy < min_energy) min_energy = energy;
         }
     }
     return min_energy;
