8ae7ebb5887b345e84ebe6ac077b91f604147127
Alex Fix <afix@cs.cornell.edu>
2014-01-11 15:06:44 -0500

    More minor changes to get it closer to compiling

--- svm-sos/svm_c++/svm_c++.cpp
+++ svm-sos/svm_c++/svm_c++.cpp
@@ -6,16 +6,11 @@ extern "C" {
 #include "svm_struct/svm_struct_common.h"
 #include "svm_struct_api.h"
 }
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
-#include "energy-common.hpp"
 #include "svm_c++.hpp"
-#include "svm_struct_options.hpp"
-#include "stats.hpp"
 
 std::unique_ptr<SVM_Cpp_Base> g_application{};
 
-void SVM_Cpp_Base::train_features(const std::string& train_file, 
+void SVM_Cpp_Base::trainFeatures(const std::string& train_file, 
         const std::string& eval_file, 
         const std::string& output_dir) {
     PatternVec train_patterns;
@@ -24,18 +19,20 @@ void SVM_Cpp_Base::train_features(const std::string& train_file,
     LabelVec eval_labels;
     readExamples(train_file, train_patterns, train_labels);
     readExamples(eval_file, eval_patterns, eval_labels);
-    initFeatures(m_derived->Params());
+    //initFeatures(m_derived->Params());
 
-    for (auto fgp : features()) {
+    for (const auto& fgp : features()) {
         fgp->Train(train_patterns, train_labels);
     }
-    for (auto fgp : features()) {
+    for (const auto& fgp : features()) {
         fgp->Evaluate(eval_patterns);
         fgp->SaveEvaluation(output_dir);
     }
 }
-template <class Derived>
-STRUCTMODEL SVM_App<Derived>::read_struct_model(char *file, STRUCT_LEARN_PARM *sparm)
-{
-}
 
+long SVM_Cpp_Base::numFeatures() const {
+    long n = 0;
+    for (const auto& fgp : features())
+        n += fgp->NumFeatures();
+    return n;
+}
